f8ad7187c02432e63ea4806ee4494be1b7a8d61d
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-03-20 13:02:28 -0700

    updated styling and added styled for all screen sizes

--- pages/index.js
+++ pages/index.js
@@ -16,118 +16,160 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="text-white bg-black text-center">
-        <header className="bg-white h-28 flex items-center justify-center space-x-16 px-12">
-          <p className="border bg-red-600 text-white rounded-xl w-16">NEW!</p>
-          <p className="w-14 text-sm text-black">
+        <header className="bg-white h-36 md:h-16 flex items-center space-x-5 px-5 md:justify-center">
+          <p className="border bg-red-600 text-white text-xs rounded-full py-1 px-3">
+            NEW!
+          </p>
+          <p className="text-base text-black text-left">
             Plans now start at{" "}
             <span className="font-bold text-base">$5.99.</span>
           </p>
           <Link href="">
-            <div className="text-[#1d63c4] flex font-bold">
+            <div className="text-[#1d63c4] flex font-bold w-32">
               <p>Learn More</p>
               <ChevronRightIcon className="w-6" />
             </div>
           </Link>
         </header>
         <div className="bg-[url('https://rb.gy/dbxdov')] bg-cover">
-          <nav className="flex justify-between pt-12 px-12">
+          <nav className="flex justify-between pt-12 px-5 xl:justify-around xl:px-0">
             <Link href="/">
               <img
                 src="https://rb.gy/ulxxee"
                 alt="Netflix Logo"
-                className="h-8"
+                className="w-24 xl:w-52"
               />
             </Link>
             <section className="flex space-x-4">
               <Link href="">
-                <section className="flex justify-around border border-white w-20 h-8 rounded-md">
-                  <GlobeAltIcon className="w-5" />
-                  <ChevronDownIcon className="w-5" />
+                <section className="flex justify-around border border-white w-14 h-8 rounded-md xl:w-32">
+                  <GlobeAltIcon className="w-5 xl:w-10" />
+                  <ChevronDownIcon className="w-5 xl:w-10" />
                 </section>
               </Link>
               <Link href="/login">
-                <button className="bg-red-600 h-8 w-20 rounded-md">
+                <button className=" bg-red-600 h-8 w-20 rounded-md font-bold xl:w-24 xl:h-9">
                   Sign In
                 </button>
               </Link>
             </section>
           </nav>
-          <section className="px-12 pt-12">
-            <h1 className="text-5xl font-bold">
+          <section className="px-12 pt-12 md:pt-32 xl:pt-40">
+            <h1 className="text-3xl font-bold md:text-4xl xl:text-6xl">
               Unlimited movies, TV shows, and more.
             </h1>
-            <p className="pt-4">Watch anywhere. Cancel anytime.</p>
-            <p className="pt-4">
+            <p className="pt-4 md:pb-2 md:text-lg xl:text-3xl">
+              Watch anywhere. Cancel anytime.
+            </p>
+            <p className="pt-4 md:text-lg xl:text-2xl">
               Ready to watch? Enter your email to create or restart your
               membership.
             </p>
-            <form className="pt-4 flex flex-col items-center">
+            <form className="pt-4 flex flex-col items-center md:flex-row md:space-x-2 md:justify-center md:pb-32">
               <input
                 placeholder="Email address"
                 type="email"
-                className=" bg-gray-600/50 border border-gray-600 placeholder:text-white pl-3 rounded-lg w-60 h-12"
+                className=" bg-gray-600/50 border border-gray-600 placeholder:text-white pl-3 rounded-lg w-60 h-12 md:w-96 xl:h-16 xl:w-[400px]"
               />
-              <button className="bg-red-600 h-12 w-40 flex items-center justify-center mt-4 rounded-md mb-8">
-                Get Started
-                <ChevronRightIcon className="w-6" />
-              </button>
+              <Link href="/login">
+                <button className="bg-red-600 font-bold h-12 w-40 flex items-center justify-center mt-4 rounded-md mb-8 md:m-0 xl:h-16 xl:w-52 xl:text-2xl">
+                  Get Started
+                  <ChevronRightIcon className="w-6" />
+                </button>
+              </Link>
             </form>
           </section>
         </div>
-        <section className="h-4 bg-[#232323]"></section>
-        <section className=" pt-12 px-12">
-          <h1 className="font-bold text-4xl pb-4">Enjoy on your TV.</h1>
-          <p>
-            Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray
-            players, and more.
-          </p>
-          <img src="https://rb.gy/qjdvyb" alt="TV" className="" />
+        <section className="break-line"></section>
+        <section className=" pt-12 px-5 md:pt-16 md:px-8 md:align-middle xl:px-28 xl:flex xl:items-center xl:py-16">
+          <div className="xl:block xl:text-left">
+            <h1 className="font-bold text-3xl pb-6 xl:text-5xl">
+              Enjoy on your TV.
+            </h1>
+            <p className="text-lg md:pb-8 xl:text-3xl">
+              Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV,
+              Blu-ray players, and more.
+            </p>
+          </div>
+          <img
+            src="https://rb.gy/qjdvyb"
+            alt="TV"
+            className="pb-12 inline-block xl:w-[450px]"
+          />
         </section>
-        <section className="h-4 bg-[#232323]"></section>
-        <section className=" pt-12 px-12">
-          <h1 className="font-bold text-4xl pb-4">Watch everywhere.</h1>
-          <p>
-            Stream unlimited movies and TV shows on your phone, tablet, laptop,
-            and TV without paying more.
-          </p>
-          <img src="https://rb.gy/ak4huh" alt="Devices" className="" />
+        <section className="break-line"></section>
+        <section className=" pt-12 px-5  xl:px-28 xl:flex xl:items-center xl:py-16 xl:flex-row-reverse xl:text-left">
+          <div>
+            <h1 className="font-bold text-4xl pb-5 xl:text-5xl">
+              Watch everywhere.
+            </h1>
+            <p className="text-lg pb-3  xl:text-3xl">
+              Stream unlimited movies and TV shows on your phone, tablet,
+              laptop, and TV without paying more.
+            </p>
+          </div>
+          <img
+            src="https://rb.gy/ak4huh"
+            alt="Devices"
+            className="pb-12 xl:w-[450px]"
+          />
         </section>
-        <section className="h-4 bg-[#232323]"></section>
-        <section className=" pt-12 px-12">
-          <h1 className="font-bold text-4xl pb-4">Create profiles for kids.</h1>
-          <p>
-            Send kids on adventures with their favorite characters in a space
-            made just for them—free with your membership.
-          </p>
-          <img src="https://rb.gy/em7vto" alt="Kids" className="" />
+        <section className="break-line"></section>
+        <section className=" pt-12 px-5 xl:px-28 xl:flex xl:items-center xl:py-16 xl:text-left">
+          <div>
+            <h1 className="font-bold text-4xl pb-4 xl:text-5xl">
+              Create profiles for kids.
+            </h1>
+            <p className="text-lg pb-3 xl:text-2xl">
+              Send kids on adventures with their favorite characters in a space
+              made just for them—free with your membership.
+            </p>
+          </div>
+          <img
+            src="https://rb.gy/em7vto"
+            alt="Kids"
+            className="pb-12 xl:w-[450px]"
+          />
         </section>
-        <section className="h-4 bg-[#232323]"></section>
-        <section className=" pt-12 px-12 flex items-center flex-col">
-          <h1 className="font-bold text-4xl pb-4">
-            Download your shows to watch offline.
-          </h1>
-          <p>Only available on ad-free plans.</p>
-          <img src="https://rb.gy/vacitf" alt="Phone" className="" />
-          <div className="flex items-center justify-around border border-gray-500 rounded-xl py-2 space-x-16 px-4 mb-20">
-            <div className="flex space-x-4">
-              <img
-                src="https://rb.gy/3el7nw"
-                alt="Stranger Things cover"
-                className="w-8"
-              />
-              <div>
-                <h2>Stranger Things</h2>
-                <p className="text-[#0071eb] text-sm text-left">
-                  Downloading...
-                </p>
+        <section className="break-line"></section>
+        <section className=" pt-12 px-5 flex items-center flex-col xl:flex-row-reverse xl:text-left xl:justify-evenly xl:pb-16">
+          <div className="">
+            <h1 className="font-bold text-4xl pb-4 xl:text-5xl xl:w-96">
+              Download your shows to watch offline.
+            </h1>
+            <p className="pb-3 xl:text-2xl">Only available on ad-free plans.</p>
+          </div>
+          <div>
+            <img
+              src="https://rb.gy/vacitf"
+              alt="Phone"
+              className="w-60 xl:w-[350px]"
+            />
+            <div className="flex items-center justify-around border border-gray-500 rounded-xl py-px space-x-16 px-4 mb-11 xl:py-4">
+              <div className="flex space-x-4 items-center">
+                <img
+                  src="https://rb.gy/3el7nw"
+                  alt="Stranger Things cover"
+                  className="w-8 h-14 xl:w-12 xl:h-26"
+                />
+                <div className="text-left">
+                  <h2 className="font-bold text-xs xl:text-lg">
+                    Stranger Things
+                  </h2>
+                  <p className="text-[#0071eb] text-xs text-left xl:text-lg">
+                    Downloading...
+                  </p>
+                </div>
               </div>
+              <FcDownload size={30} />
             </div>
-            <FcDownload size={30} />
           </div>
         </section>
-        <section className="h-4 bg-[#232323]"></section>
-        <section className="pt-12">
-          <h1 className="text-4xl pb-10">Frequently Asked Questions</h1>
+        <section className="break-line"></section>
+        <section className="pt-12 px-5 xl:px-28">
+          <h1 className="text-3xl font-bold pb-5 md:pt-2 xl:text-5xl xl:pb-10">
+            Frequently Asked Questions
+          </h1>
           <div className="asked-questions">
             <h1>What is Netflix?</h1>
             <PlusSmallIcon className="w-10" />
@@ -152,11 +194,26 @@ export default function Home() {
             <h1>What can I watch on Netflix?</h1>
             <PlusSmallIcon className="w-10" />
           </div>
-          <div className="asked-questions">
+          <div className="asked-questions mb-12">
             <h1>Is Netflix good for kids?</h1>
             <PlusSmallIcon className="w-10" />
           </div>
+          <p className="pb-4 px-9 text-lg">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+          <form className="flex flex-col items-center pb-12 md:flex-row md:justify-center md:space-x-2">
+            <input
+              placeholder="Email address"
+              type="email"
+              className=" bg-gray-600/50 border border-gray-600 placeholder:text-gray-300 pl-3 rounded-lg pr-8 mb-4 h-12 md:mb-0 md:pr-52"
+            />
+            <button className=" bg-red-600 px-5 py-3 rounded-md font-bold flex items-center md:px-8 md:text-lg">
+              Get Started <ChevronRightIcon className="w-6" />
+            </button>
+          </form>
         </section>
+        <section className="break-line"></section>
       </main>
     </>
   );
