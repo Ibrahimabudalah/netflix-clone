828259261666f80561c306647faf429658f3d271
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-03-29 23:50:35 -0700

    created a dynamic route page to view movie details for every movie selected, used the next router hook with the query param to pass the movie id then fetch the trailers for that movie using the id

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,19 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Thumbnail({ movie }) {
-  //   console.log(movie);
+  // console.log(movie);
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
-      <Image
-        key={movie.id}
-        src={`https://image.tmdb.org/t/p/w500${
-          movie.backdrop_path || movie.poster_path
-        }`}
-        className="rounded-sm object-cover md:rounded"
-        fill
-        alt={movie.title || "Movie Poster"}
-      />
-    </div>
+    <Link
+      href={{
+        pathname: "/[id]", // the path which will be the [id] page
+        query: movie, // the movie data I want to pass to the dynamic page [id]
+      }}
+    >
+      <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+        <Image
+          key={movie.id}
+          src={`https://image.tmdb.org/t/p/w500${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          className="rounded-sm object-cover md:rounded"
+          fill
+          alt={movie.title || "Movie Poster"}
+        />
+      </div>
+    </Link>
   );
 }
 
---
+++ pages/[id].js
@@ -0,0 +1,35 @@
+import Navbar from "@/components/Navbar";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+function Movie() {
+  const router = useRouter();
+  const query = router.query;
+  const [data, setData] = useState(null);
+
+  const BASE_URL = "https://api.themoviedb.org/3";
+  useEffect(() => {
+    if (!query) {
+      console.log(data); // if NOT
+    } else {
+      fetch(
+        `${BASE_URL}/movie/${query.id}/videos?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setData(data.results);
+        });
+    }
+    //the above fetch is used to fetch all trailers corresponding with the movie ID using the id obtained from the router query
+  }, [router.isReady]);
+  // console.log(data)
+  // console.log(query)
+  return (
+    <div>
+      <Navbar />
+      <h1>{query.original_title || query.name || query.original_name}</h1>
+    </div>
+  );
+}
+
+export default Movie;
